f2ef7d791fb3e81126536f5b3d376ef1736fe1e0
Felipe Nascimento <flpnascto@gmail.com>
2021-08-18 16:29:10 -0300

    requisito 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_lines = []
+    for index in range(0, len(instance)):
+        item = instance.search(index)
+        occurrence = []
+        is_word_found = False
+        for line in item["linhas_do_arquivo"]:
+            if word.lower() in line.lower():
+                occurrence.append(
+                    {"linha": item["linhas_do_arquivo"].index(line) + 1}
+                )
+                is_word_found = True
+        if is_word_found:
+            word_lines.append(
+                {
+                    "palavra": word,
+                    "arquivo": item["nome_do_arquivo"],
+                    "ocorrencias": occurrence,
+                }
+            )
+    return word_lines
 
 
 def search_by_word(word, instance):
