c17e09d924c49751e088292505524aec3a579e48
Felipe Nascimento <flpnascto@gmail.com>
2021-08-18 15:03:16 -0300

    requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    lines = []
+    try:
+        if not path_file.endswith(".txt"):
+            sys.stderr.write("Formato inválido\n")
+        else:
+            with open(path_file, "rt", newline="\n") as txt_content:
+                for line in txt_content:
+                    lines.append(line.strip())
+                return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
