f90e71b6947b3da332f41826cf12bf51453979b0
Felipe Nascimento <flpnascto@gmail.com>
2021-08-20 20:14:58 -0300

    change attributes to private

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,25 +1,25 @@
 class Queue:
     def __init__(self):
-        self._data = []
-        self._data_len = 0
+        self.__data = []
+        self.__data_len = 0
 
     def __len__(self):
-        return self._data_len
+        return self.__data_len
 
     def enqueue(self, value):
-        self._data.insert(0, value)
-        self._data_len += 1
+        self.__data.insert(0, value)
+        self.__data_len += 1
 
     def dequeue(self):
-        if self._data:
-            self._data_len -= 1
-            return self._data.pop()
+        if self.__data:
+            self.__data_len -= 1
+            return self.__data.pop()
 
     def search(self, index):
         # if self._data[index]:
         #     raise IndexError
         # else:
-        return self._data[len(self._data) - index - 1]
+        return self.__data[len(self.__data) - index - 1]
 
     def __str__(self):
-        return "Queue(" + ", ".join(str(x) for x in self._data) + ")"
+        return "Queue(" + ", ".join(str(x) for x in self.__data) + ")"
